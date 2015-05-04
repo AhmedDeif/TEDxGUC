10698e9dcbda2a33e320119f40ad4e217623ffec
Doola <ahmed.adel.abodeif@gmail.com>
2015-05-04 17:40:58 +0300

    the batman

---
+++ tedxApp/app/controllers/batman_controller.rb
@@ -0,0 +1,10 @@
+class BatmanController < ApplicationController
+  def index
+  end
+  def page2
+  end
+  def page3
+  end
+  def page4
+  end	
+end
--- tedxApp/config/routes.rb
+++ tedxApp/config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
+  get 'static_pages/home'
+
+  get 'static_pages/help'
+
+  get 'batman/index'
+
+  get 'batman/page2'
+
+  get 'batman/page3'
+
+  get 'batman/page4'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
