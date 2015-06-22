7515370f81b8924e5cc0c42f25c41e57187ac467
Nadine Mansour <nadinemansour94@gmail.com>
2015-06-22 15:23:37 +0200

    adding routs for coldplay view

--- tedxApp/config/routes.rb
+++ tedxApp/config/routes.rb
@@ -41,6 +41,8 @@ Rails.application.routes.draw do
 
   get 'batman/page4'
 
+  get 'coldplay/home'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
